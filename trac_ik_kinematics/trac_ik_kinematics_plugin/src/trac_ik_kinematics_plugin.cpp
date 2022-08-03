de42a24b871a868a64449957ed961c319fb6be07
JD Yamokoski <jyamokoski@nauticusrobotics.com>
2022-08-03 03:06:58 +0000

    * Cleans up dependencies * Makes better use of tf2_kdl functionality * Adds a few more log messages * Fixes undefined chain object

--- trac_ik_kinematics/trac_ik_kinematics_plugin/src/trac_ik_kinematics_plugin.cpp
+++ trac_ik_kinematics/trac_ik_kinematics_plugin/src/trac_ik_kinematics_plugin.cpp
@@ -28,60 +28,41 @@ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISE
 OF THE POSSIBILITY OF SUCH DAMAGE.
 ********************************************************************************/
 
-// ROS
-#include <rclcpp/rclcpp.hpp>
-#include <random_numbers/random_numbers.h>
-
-#include <urdf/model.h>
 #include <algorithm>
+#include <limits>
 
 // KDL
 #include <kdl/tree.hpp>
 #include <kdl_parser/kdl_parser.hpp>
-#include <kdl_parser/kdl_parser.hpp>
-#include <kdl/chainfksolverpos_recursive.hpp>
-#include <kdl/frames_io.hpp>
-#include <kdl/kinfam_io.hpp>
+#include <tf2_kdl/tf2_kdl.h>
 
 #include <trac_ik/trac_ik.hpp>
 #include <trac_ik_kinematics_plugin/trac_ik_kinematics_plugin.h>
-#include <limits>
 
-void poseMsgToKDL(const geometry_msgs::msg::Pose &m, KDL::Frame &k)
-{
-  k.p[0] = m.position.x;
-  k.p[1] = m.position.y;
-  k.p[2] = m.position.z;
-
-  k.M = KDL::Rotation::Quaternion(m.orientation.x, m.orientation.y, m.orientation.z, m.orientation.w);
-}
-
-void poseKDLToMsg(const KDL::Frame &k, geometry_msgs::msg::Pose &m)
-{
-  m.position.x = k.p[0];
-  m.position.y = k.p[1];
-  m.position.z = k.p[2];
-
-  k.M.GetQuaternion(m.orientation.x, m.orientation.y, m.orientation.z, m.orientation.w);
-}
 
 namespace trac_ik_kinematics_plugin
 {
-  static rclcpp::Logger LOGGER = rclcpp::get_logger("moveit_trac_ik_kinematics_plugin.trac_ik_kinematics_plugin");
+  static rclcpp::Logger LOGGER = rclcpp::get_logger("trac_ik_kinematics_plugin");
 
   TRAC_IKKinematicsPlugin::TRAC_IKKinematicsPlugin() : initialized_(false)
   {
   }
 
-  bool TRAC_IKKinematicsPlugin::initialize(const rclcpp::Node::SharedPtr &node, const moveit::core::RobotModel &robot_model,
-                                           const std::string &group_name, const std::string &base_frame,
-                                           const std::vector<std::string> &tip_frames, double search_discretization)
+  bool TRAC_IKKinematicsPlugin::initialize(const rclcpp::Node::SharedPtr &node, 
+                                           const moveit::core::RobotModel &robot_model,
+                                           const std::string &group_name, 
+                                           const std::string &base_frame,
+                                           const std::vector<std::string> &tip_frames, 
+                                           double search_discretization)
   {
     node_ = node;
     storeValues(robot_model, group_name, base_frame, tip_frames, search_discretization);
     joint_model_group_ = robot_model_->getJointModelGroup(group_name);
     if (!joint_model_group_)
+    {
+      RCLCPP_ERROR_STREAM(LOGGER, "Unknown planning group: " << group_name);
       return false;
+    }
 
     if (!joint_model_group_->isChain())
     {
@@ -149,7 +130,25 @@ namespace trac_ik_kinematics_plugin
     // ###
 
     lookupParam(node_, "position_only_ik", position_ik_, false);
-    lookupParam(node_, "solve_type", solve_type, std::string("Speed"));
+    RCLCPP_INFO_STREAM(LOGGER, "Position only IK: " << (position_ik_ ? "TRUE" : "FALSE"));
+    std::string solve_type_str;
+    lookupParam(node_, "solve_type", solve_type_str, std::string("Speed"));
+    if (solve_type_str == "Manipulation1")
+      solve_type_ = TRAC_IK::Manip1;
+    else if (solve_type_str == "Manipulation2")
+      solve_type_ = TRAC_IK::Manip2;
+    else if (solve_type_str == "Distance")
+      solve_type_ = TRAC_IK::Distance;
+    else
+    {
+      if (solve_type_str != "Speed")
+      {
+        RCLCPP_WARN_STREAM(LOGGER, solve_type_str << " is not a valid solve_type; setting to default: Speed");
+        solve_type_str = "Speed";
+      }
+      solve_type_ = TRAC_IK::Speed;
+    }
+    RCLCPP_INFO_STREAM(LOGGER, "Solver type: " << solve_type_str);
 
     initialized_ = true;
     return true;
@@ -158,9 +157,17 @@ namespace trac_ik_kinematics_plugin
   int TRAC_IKKinematicsPlugin::getKDLSegmentIndex(const std::string &name) const
   {
     int i = 0;
-    while (i < (int)chain.getNrOfSegments())
+    // while (i < (int)chain.getNrOfSegments())
+    // {
+    //   if (chain.getSegment(i).getName() == name)
+    //   {
+    //     return i + 1;
+    //   }
+    //   i++;
+    // }
+    while (i < (int)kdl_chain_.getNrOfSegments())
     {
-      if (chain.getSegment(i).getName() == name)
+      if (kdl_chain_.getSegment(i).getName() == name)
       {
         return i + 1;
       }
@@ -195,15 +202,18 @@ namespace trac_ik_kinematics_plugin
       jnt_pos_in(i) = joint_angles[i];
     }
 
-    KDL::ChainFkSolverPos_recursive fk_solver(chain);
+    // KDL::ChainFkSolverPos_recursive fk_solver(chain);
+    // KDL::ChainFkSolverPos_recursive fk_solver(kdl_chain_);
 
     bool valid = true;
     for (unsigned int i = 0; i < poses.size(); i++)
     {
       RCLCPP_DEBUG(LOGGER, "End effector index: %d", getKDLSegmentIndex(link_names[i]));
-      if (fk_solver.JntToCart(jnt_pos_in, p_out, getKDLSegmentIndex(link_names[i])) >= 0)
+      // if (fk_solver.JntToCart(jnt_pos_in, p_out, getKDLSegmentIndex(link_names[i])) >= 0)
+      if (fk_solver_->JntToCart(jnt_pos_in, p_out, getKDLSegmentIndex(link_names[i])) >= 0)
       {
-        poseKDLToMsg(p_out, poses[i]);
+        // poseKDLToMsg(p_out, poses[i]);
+        poses[i] = tf2::toMsg(p_out);
       }
       else
       {
@@ -337,7 +347,7 @@ namespace trac_ik_kinematics_plugin
     }
 
     KDL::Frame frame;
-    poseMsgToKDL(ik_pose, frame);
+    tf2::fromMsg(ik_pose, frame);
 
     KDL::JntArray in(num_joints_), out(num_joints_);
 
@@ -355,24 +365,7 @@ namespace trac_ik_kinematics_plugin
 
     double epsilon = 1e-5; //Same as MoveIt's KDL plugin
 
-    TRAC_IK::SolveType solvetype;
-
-    if (solve_type == "Manipulation1")
-      solvetype = TRAC_IK::Manip1;
-    else if (solve_type == "Manipulation2")
-      solvetype = TRAC_IK::Manip2;
-    else if (solve_type == "Distance")
-      solvetype = TRAC_IK::Distance;
-    else
-    {
-      if (solve_type != "Speed")
-      {
-        RCLCPP_WARN_STREAM(LOGGER, solve_type << " is not a valid solve_type; setting to default: Speed");
-      }
-      solvetype = TRAC_IK::Speed;
-    }
-
-    TRAC_IK::TRAC_IK ik_solver(kdl_chain_, joint_min_, joint_max_, timeout, epsilon, solvetype);
+    TRAC_IK::TRAC_IK ik_solver(kdl_chain_, joint_min_, joint_max_, timeout, epsilon, solve_type_);
 
     int rc = ik_solver.CartToJnt(in, frame, out, bounds);
 
@@ -394,7 +387,7 @@ namespace trac_ik_kinematics_plugin
         }
         else
         {
-          RCLCPP_DEBUG(LOGGER, "Solution has error code %i", error_code);
+          RCLCPP_DEBUG(LOGGER, "Solution has error code %d", error_code.val);
           return false;
         }
       }
@@ -418,6 +411,11 @@ namespace trac_ik_kinematics_plugin
 
 } // end namespace
 
-//register TRAC_IKKinematicsPlugin as a KinematicsBase implementation
-#include <class_loader/class_loader.hpp>
-CLASS_LOADER_REGISTER_CLASS(trac_ik_kinematics_plugin::TRAC_IKKinematicsPlugin, kinematics::KinematicsBase)
+// //register TRAC_IKKinematicsPlugin as a KinematicsBase implementation
+// #include <class_loader/class_loader.hpp>
+// CLASS_LOADER_REGISTER_CLASS(trac_ik_kinematics_plugin::TRAC_IKKinematicsPlugin, kinematics::KinematicsBase)
+
+// register TRAC_IKKinematicsPlugin as a KinematicsBase implementation
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(trac_ik_kinematics_plugin::TRAC_IKKinematicsPlugin, kinematics::KinematicsBase);
+
